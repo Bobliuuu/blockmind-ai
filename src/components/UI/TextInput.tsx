2191ff464a8780de4a1af980ded7df1e7226abd5
Andrew Qiao <andrewqiao2004@gmail.com>
2023-06-23 22:46:45 -0400

    add text input component and start log in page

---
+++ src/components/UI/TextInput.tsx
@@ -0,0 +1,53 @@
+interface TextInputProps {
+  id: string;
+  name: string;
+  type: string;
+  placeholder: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onBlur: (e: React.FocusEvent<HTMLInputElement>) => void;
+  padding?: string;
+  icon?: React.ReactNode;
+  iconOnClick?: () => void;
+  classes?: string;
+}
+
+export default function TextInput({
+  id,
+  name,
+  type,
+  placeholder,
+  value,
+  onChange,
+  onBlur,
+  padding = "px-5 py-4.5",
+  icon,
+  iconOnClick,
+  classes,
+}: TextInputProps) {
+  return (
+    <div className={`relative ${classes || ""}`}>
+      <input
+        id={id}
+        name={name}
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        onBlur={onBlur}
+        className={`transition-300 inline-block w-full rounded-md bg-purple3 text-white outline-none placeholder:text-beige ${padding} ${
+          icon ? "pr-14" : ""
+        }`}
+      />
+      {icon && (
+        <button
+          type="button"
+          onClick={iconOnClick}
+          className="absolute right-5 top-1/2 -translate-y-1/2 cursor-pointer"
+        >
+          {icon}
+        </button>
+      )}
+    </div>
+  );
+}
--- src/pages/log-in.tsx
+++ src/pages/log-in.tsx
@@ -1,9 +1,43 @@
+import { useState } from "react";
+import Link from "next/link";
 import AuthLayout from "~/components/layouts/AuthLayout";
+import { useFormik } from "formik";
 
 export default function LogIn() {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
+  const handleGoogleLogin = () => {
+    console.log("google login");
+  };
+
+  const handlWorldCoinLogIn = () => {
+    console.log("worldcoin login");
+  };
+
   return (
     <AuthLayout>
-      <div className=""></div>
+      <h1 className="mb-3 font-display text-6xl font-bold text-white 2xs:text-7xl xl:text-8xl 2xl:text-9xl">
+        Log In
+      </h1>
+      <p className="mb-7 text-beige md:text-lg xl:mb-10">
+        Don&apos;t have an account?{" "}
+        <Link href="/sign-up" className="text-gradient">
+          Sign Up.
+        </Link>
+      </p>
+      <form onSubmit={formik.handleSubmit}>
+        <div className="mb-5 md:mb-6"></div>
+      </form>
     </AuthLayout>
   );
 }
