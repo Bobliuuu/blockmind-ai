d3b1b280716bd71e1381dc764d965f2d332a6f08
Andrew Qiao <andrewqiao2004@gmail.com>
2023-06-23 22:34:17 -0400

    finish dashboard menu

--- src/components/navigation/DashboardMenu.tsx
+++ src/components/navigation/DashboardMenu.tsx
@@ -1,5 +1,11 @@
 import { useRouter } from "next/router";
-import { MessageSquare } from "react-feather";
+import {
+  LogOut,
+  MessageSquare,
+  Repeat,
+  Settings,
+  TrendingUp,
+} from "react-feather";
 import { useDispatch, useSelector } from "react-redux";
 import { closeMobileMenu } from "~/store/slices/mobileMenuSlice";
 import { type AppDispatch, type RootState } from "~/store/store";
@@ -13,9 +19,36 @@ export default function DashboardMenu() {
     (state: RootState) => state.mobileMenu.isOpen
   );
 
+  const bottomMenu = [
+    {
+      label: "Market Visualizer",
+      icon: <TrendingUp size={24} color={COLORS.white} />,
+      isActive: pathname === "/dashboard/markets",
+      onClick: () => router.push("/dashboard/markets"),
+    },
+    {
+      label: "Send / Swap",
+      icon: <Repeat size={24} color={COLORS.white} />,
+      isActive: pathname === "/dashboard/send-swap",
+      onClick: () => router.push("/dashboard/send-swap"),
+    },
+    {
+      label: "Settings",
+      icon: <Settings size={24} color={COLORS.white} />,
+      isActive: pathname === "/dashboard/settings",
+      onClick: () => router.push("/dashboard/settings"),
+    },
+    {
+      label: "Log Out",
+      icon: <LogOut size={24} color={COLORS.white} />,
+      isActive: false,
+      onClick: () => router.push("/"),
+    },
+  ];
+
   return (
     <nav
-      className={`transition-300 h-100dvh fixed inset-0 z-30 flex w-full flex-col bg-purple3 px-2 pb-4 pt-28 xs:px-5 sm:px-7 lg:pb-8 lg:pt-32 xl:w-[300px] xl:px-4 ${
+      className={`transition-300 h-100dvh fixed inset-0 z-30 flex w-full flex-col bg-purple3 px-2 pb-8 pt-28 xs:px-5 sm:px-7 lg:pt-32 xl:w-[300px] xl:px-4 ${
         isMobileMenuOpen ? "translate-x-0" : "translate-x-full xl:translate-x-0"
       }`}
     >
@@ -28,6 +61,33 @@ export default function DashboardMenu() {
         classes="bg-purple2 bg-opacity-20"
       />
       <div className="bg-gradient mt-3 h-0.25" />
+      <div className="relative flex flex-grow flex-col overflow-y-hidden">
+        <div className="no-scrollbar flex flex-grow flex-col gap-1 overflow-y-scroll py-3 pb-8">
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) => (
+            <DashboardMenuItem
+              key={i}
+              label={`Chat ${i}`}
+              isActive={false}
+              // eslint-disable-next-line @typescript-eslint/no-misused-promises
+              onClick={() => router.push("/dashboard")}
+            />
+          ))}
+        </div>
+        <div className="pointer-events-none absolute inset-x-0 bottom-0 h-16 bg-gradient-to-b from-transparent to-purple3"></div>
+      </div>
+      <div className="bg-gradient mb-3 h-0.25" />
+      <div className="flex flex-shrink-0 flex-col gap-1">
+        {bottomMenu.map((item) => (
+          <DashboardMenuItem
+            key={item.label}
+            label={item.label}
+            icon={item.icon}
+            isActive={item.isActive}
+            // eslint-disable-next-line @typescript-eslint/no-misused-promises
+            onClick={item.onClick}
+          />
+        ))}
+      </div>
     </nav>
   );
 }
