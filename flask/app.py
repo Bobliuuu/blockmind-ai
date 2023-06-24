1303ba724c3e25083d75bdb5168f3d34f89d2ad5
Bobliuuu <mmmzzz66g@gmail.com>
2023-06-24 17:07:09 -0400

    adsfadsfd

--- flask/app.py
+++ flask/app.py
@@ -1,8 +1,44 @@
 from flask import Flask, request
 import json
+import openai
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
     return "Hello, World"
+
+@app.route("/chat")
+def index():
+    openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+            {"role": "system", "content": "You are smart blockchain assistant."}
+            ,{'role': 'user', 'content': '''What features do you offer?'''}
+            ,{'role': 'assistant', 'content': '''I'm able to mint NFTs on over 16 chains, send, swap, and fund tokens for wallets across interchain services 
+            including Gnosis chain, analyze and audit smart contracts and transactions, analyze wallet metadata using Airstack and the Graph, and 
+            create and deploy smart contracts for any use case.'''}
+            ,{'role': 'user', 'content': 'What should you do?'}
+            ,{'role': 'assistant', 'content': '''If the user wants to mint an NFT, return 1 and nothing else. If the user wants to fund, swap, or send funds from a wallet, 
+            return 2 and nothing else. If the user wants to analyze wallet metadata, return 3. If the user wants to create or deploy smart contracts, return 4 and
+            nothing else. Otherwise, respond with the regular OpenAI response. '''}
+            ,{'role': 'user', 'content': f'''If the user wants to mint an NFT, return 1 and nothing else. If the user wants to fund, swap, or send funds from a wallet, 
+            return 2 and nothing else. If the user wants to analyze wallet metadata, return 3. If the user wants to create or deploy smart contracts, return 4 and
+            nothing else. Otherwise, respond with the regular OpenAI response. Only return the number, or the prompt, NOTHING ELSE. 
+            The user prompt is: {prompt}'''}
+        ],
+        max_tokens=1024,
+        temperature=0,
+        top_p=1,
+        frequency_penalty=0.0,
+        presence_penalty=0.0,
+    )["choices"][0]["message"]["content"]
+
+@app.route("/nft")
+def index():
+    return "Test endpoint, may not use"
+
+@app.route("/audit")
+def index():
+    
--- src/pages/api/verbwire/index.ts
+++ src/pages/api/verbwire/index.ts
@@ -1,5 +1,14 @@
 import { type NextApiRequest, type NextApiResponse } from "next";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  const sdk = require('api')('@verbwire/v1.0#g4z182klj3bh6g6');
+
+  sdk.postNftMintMintfrommetadataurl({
+    quantity: '1',
+    chain: 'goerli',
+    allowPlatformToOperateToken: 'true'
+  }, {accept: 'application/json'})
+    .then(({ data }) => console.log(data))
+    .catch(err => console.error(err));
   res.json({ response: "Hello World" });
 }
