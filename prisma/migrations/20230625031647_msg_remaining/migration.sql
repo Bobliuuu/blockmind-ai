698c90557ba1add3f5f552ebd3c32468f99d519c
Amol Venkataraman <amolven16@gmail.com>
2023-06-24 23:17:21 -0400

    Updated some user stuff

---
+++ prisma/migrations/20230625031647_msg_remaining/migration.sql
@@ -0,0 +1,17 @@
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT,
+    "email" TEXT,
+    "emailVerified" DATETIME,
+    "image" TEXT,
+    "msg_allowed" INTEGER NOT NULL DEFAULT 0,
+    "is_admin" BOOLEAN NOT NULL DEFAULT false
+);
+INSERT INTO "new_User" ("email", "emailVerified", "id", "image", "name") SELECT "email", "emailVerified", "id", "image", "name" FROM "User";
+DROP TABLE "User";
+ALTER TABLE "new_User" RENAME TO "User";
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
--- src/pages/dashboard/account.tsx
+++ src/pages/dashboard/account.tsx
@@ -1,17 +1,55 @@
 import Button from "~/components/UI/Button";
-import { signOut } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
 import { useRouter } from "next/router";
+import Loading from "~/components/sections/loading/loading";
+import Unauthenticated from "~/components/sections/error/unauthenticated";
+import Image from "next/image";
+import googleIcon from "~/../public/icons/google-card.png";
+import { ShoppingCart } from "react-feather";
 
 export default function Settings() {
   const router = useRouter();
 
+  const { data: session, status } = useSession();
+
+  if (status === "loading") {
+    return <Loading />
+  } else if (status === "unauthenticated") {
+    return <Unauthenticated />
+  }
+
   return (
     <div className="mx-dashboard mt-32">
       <h1 className="mb-6 font-display text-4xl font-bold text-white md:text-5xl xl:mb-8 xl:text-6xl">
-        Account
+        Welcome {session?.user.name}
       </h1>
-      <h4>TODO Here</h4>
-      <p>User account information, Type of account, Remaining free queries, Place to buy more queries, Wallet Connection</p>
+
+      <div className="rounded mb-4 flex max-w-[400px] overflow-hidden rounded-md border border-purple2">
+        <div className="w-[200px] px-5 py-4.5 text-white text-opacity-60 border-[0] border-r border-solid border-purple2">
+          <center>
+            <h3 className="mb-4 text-xl font-semibold text-white">Account Type</h3>
+          </center>
+          <Image src={googleIcon} alt="Google Icon" className="inline-block" />
+        </div>
+        <div className="w-[200px] px-5 py-4.5 text-white text-opacity-60">
+          <center>
+            <h3 className="mb-4 text-xl font-semibold text-white">Remaining Quota</h3>
+          </center>
+          <center>
+            <span className="font-display text-12xl font-bold text-white md:text-14xl xl:text-16xl">30</span>
+          </center>
+        </div>
+      </div>
+      <Button
+        type="submit"
+        hierarchy="primary"
+        font="font-semibold"
+        classes="w-full mb-8 md:mb-8 w-[400px]"
+        icon={<ShoppingCart size={48} className="w-5 xs:w-6 lg:w-7" />}
+      >
+        Buy More Queries
+      </Button>
+
       <h3 className="mb-4 text-xl font-semibold text-white">
         Connected Wallet
       </h3>
